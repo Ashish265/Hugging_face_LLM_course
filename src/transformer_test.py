b14cd20d8cd605fe282cbd9b33cfc17540aa2183
Ashish <ashishgupta2692@gmail.com>
2025-10-24 14:13:37 +0000

    updated code

--- src/transformer_test.py
+++ src/transformer_test.py
@@ -1,4 +1,4 @@
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
-classifier("I've been waiting for a huggingface course my whole life")
+print(classifier("I've been waiting for a huggingface course my whole life"))
