367f100ad848cc7fd377668e904eae6cd9630555
Ashish <ashishgupta2692@gmail.com>
2025-10-25 16:49:42 +0000

    hugging face pipeline

--- src/transformer_test.py
+++ src/transformer_test.py
@@ -2,3 +2,12 @@ from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
 print(classifier("I've been waiting for a huggingface course my whole life"))
+
+
+classifier = pipeline("zero-shot-classification")
+print(
+    classifier(
+        """This is s course about the transformer library""",
+        candidate_labels=["education", "politics", "business"],
+    )
+)
